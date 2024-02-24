801aa78db9bd365124b853ddc6bc2cfba505a257
zoelesv <zoomlee1141@gmail.com>
2024-02-24 15:00:07 -0800

    edit init

--- frontend_component/__init__.py
+++ frontend_component/__init__.py
@@ -4,12 +4,11 @@ import streamlit as st
 import time
 import pandas as pd
 import streamlit_shadcn_ui as ui
+from my_component.backend import *
+from backend import *
+import matplotlib.pyplot as plt
+import numpy as np
 
-output = {'Confidence Level': 0.95,
- 'Minimum Detectable Effect (MDE)': 0.1,
- 'Statistical Power': 0.8,
- 'Test Type': 'Two-sided',
- 'Sample Size': 392}
 st.set_page_config(
     page_title="AB Experiment",
     layout="wide"
@@ -27,15 +26,46 @@ st.markdown("""<style>
             .appview-container .main .block-container{{padding-left: 0; padding-top: 0;}}
             </style>""", unsafe_allow_html=True)
 col1, col2 = st.columns([1,2])
-# Take query and upload a file
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(df, bins=20)
 
-col1.markdown("# Welcome to AB Experiment")
-col1.chat_input("Type your query")
+st.pyplot(fig)
+# Take query and upload a file
+col2.line_chart(data=df, x='variation', y='browse_time')
+col1.markdown(" #Welcome to AB Experiment")
+input = col1.chat_input("Type your query")
+#output = get_backend_response(input, csv=False)
+output = {'Confidence Level': 0.95,
+ 'Minimum Detectable Effect (MDE)': 0.1,
+ 'Statistical Power': 0.8,
+ 'Test Type': 'Two-sided',
+ 'Sample Size': 392}
 def change_csv_state():
     st.session_state["csv"]="done"
-    st.session_state["log"]="done"
 
 csv_file = col1.file_uploader("Upload a file", on_change= change_csv_state)
+st.session_state["csv"]="none"
+
+col2.markdown(" #Statistics go here")
+col2.metric(label="Confidence Level", value=output['Confidence Level'])
+col2.metric(label="Minimum Detectable Effect (MDE)", value=output['Minimum Detectable Effect (MDE)'])
+#col1.slider(label="power")
+col2.scatter_chart(data=df, x='variation', y='browse_time')
+#col2.plotly_chart(figure_or_data=plotly.graph_objs.Data)
+#ui.table(data=df, maxHeight=300)
+
+if st.session_state["csv"] == "done":
+    progress_bar = col1.progress(0)
+    for perc_completed in range(100):
+        time.sleep(0.01)
+        progress_bar.progress(perc_completed+1)
+    col1.success("Uploaded successfully!")
+    df = pd.read_csv(csv_file)
+    col1.bar_chart(data=df, x='variation', y='browse_time')
+
+
+#_RELEASE = False
 
 def my_component(name, key=None):
     """Create a new instance of "my_component".
@@ -70,7 +100,6 @@ def my_component(name, key=None):
     # There's no need to do this in our simple example - but it's an option.
     return component_value
 
-
 col2.markdown("# Statistics go here")
 with col2.container():
     _component_func = components.declare_component(
@@ -80,26 +109,3 @@ with col2.container():
 
     # name_input = st.text_input("Enter a name", value="Streamlit")
     my_component(name = list(output.values()), key=list(output.keys()))
-
-#col1.slider(label="power")
-
-#col2.plotly_chart(figure_or_data=plotly.graph_objs.Data)
-#ui.table(data=df, maxHeight=300)
-
-#if st.session_state["csv"] == "done":
-    #progress_bar = col1.progress(0)
-    #for perc_completed in range(100):
-        #time.sleep(0.01)
-        #progress_bar.progress(perc_completed+1)
-    #col1.success("Uploaded successfully!")
-    #components.html(get_pyg_html(df), width=1300, height=1000, scrolling=True)
-    #col3.metric(label="Temperature", value="60 °C", delta="3 °C") 
-    #with st.expander("Click to read more"):
-        #st.write("Hello, here are more details on this topic that you were interested in.")
-
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-
