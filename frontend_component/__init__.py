a9442439eb50d70e651ae572b5817eea0daa00c4
zoelesv <zoomlee1141@gmail.com>
2024-02-24 16:14:21 -0800

    edit init

--- frontend_component/__init__.py
+++ frontend_component/__init__.py
@@ -6,6 +6,8 @@ import pandas as pd
 import streamlit_shadcn_ui as ui
 import matplotlib.pyplot as plt
 import numpy as np
+from backend import *
+import plotly.figure_factory as ff
 
 st.set_page_config(
     page_title="AB Experiment",
@@ -18,40 +20,39 @@ def get_df() -> pd.DataFrame:
 
 df = get_df()
 
-st.title("ABA")
+st.title("AB Agent")
 
 st.markdown("""<style>
             .appview-container .main .block-container{{padding-left: 0; padding-top: 0;}}
             </style>""", unsafe_allow_html=True)
 col1, col2 = st.columns([1,2])
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(df, bins=20)
-
-st.pyplot(fig)
+if "csv" not in st.session_state:
+    st.session_state["csv"]="none"
 # Take query and upload a file
 col2.markdown("# Statistics go here")
-col2.line_chart(data=df, x='variation', y='browse_time')
+#col2.line_chart(data=df, x='variation', y='browse_time')
 col1.markdown("# Welcome to AB Experiment")
 input = col1.chat_input("Type your query")
-output = {'Confidence Level': 0.95,
- 'Minimum Detectable Effect (MDE)': 0.1,
- 'Statistical Power': 0.8,
- 'Test Type': 'Two-sided',
- 'Sample Size': 392}
+if "prompt" not in st.session_state:
+    st.session_state["prompt"]="none"
 def change_csv_state():
     st.session_state["csv"]="done"
 
-csv_file = col1.file_uploader("Upload a file", on_change= change_csv_state)
-st.session_state["csv"]="none"
+csv_file = col1.file_uploader("Upload a file", on_change = change_csv_state)
 
-col2.metric(label="Confidence Level", value=output['Confidence Level'])
-col2.metric(label="Minimum Detectable Effect (MDE)", value=output['Minimum Detectable Effect (MDE)'])
-#col1.slider(label="power")
-col2.scatter_chart(data=df, x='variation', y='browse_time')
+#col2.scatter_chart(data=df, x='variation', y='browse_time')
 #col2.plotly_chart(figure_or_data=plotly.graph_objs.Data)
 #ui.table(data=df, maxHeight=300)
-
+if input and st.session_state["csv"]=="none":
+    try:
+        output = get_backend_response(input, False)
+        st.session_state["prompt"]="done"
+    except:
+        output = {'Confidence Level': 0.95,
+    'Minimum Detectable Effect (MDE)': 0.1,
+    'Statistical Power': 0.8,
+    'Test Type': 'Two-sided',
+    'Sample Size': 392}
 if st.session_state["csv"] == "done":
     progress_bar = col1.progress(0)
     for perc_completed in range(100):
@@ -59,7 +60,23 @@ if st.session_state["csv"] == "done":
         progress_bar.progress(perc_completed+1)
     col1.success("Uploaded successfully!")
     df = pd.read_csv(csv_file)
-    col1.bar_chart(data=df, x='variation', y='browse_time')
+    #col1.bar_chart(data=df, x='variation', y='browse_time')
+    #st.plotly_chart(figure_or_data=df)
+    # Add histogram data
+    x1 = df[df['variation'] == 'A']['browse_time']
+    x2 = df[df['variation'] == 'B']['browse_time']
+
+    # Group data together
+    hist_data = [x1, x2]
+
+    group_labels = ['A', 'B']
+
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(
+            hist_data, group_labels, bin_size=[.1, .25, .5])
+
+    # Plot!
+    col1.plotly_chart(fig, use_container_width=True)
 
 
 #_RELEASE = False
@@ -102,6 +119,11 @@ with col2.container():
         "my_component",
         url="http://localhost:3001",
     )
-
-    # name_input = st.text_input("Enter a name", value="Streamlit")
-    my_component(name = list(output.values()), key=list(output.keys()))
+    if st.session_state["prompt"]=="done":
+        # name_input = st.text_input("Enter a name", value="Streamlit")
+        my_component(name = list(output.values()), key=list(output.keys()))
+    else:
+        my_component(name = [], key=[])
+    if st.session_state["csv"] == "done":
+        output = get_backend_response('', True)
+        col2.markdown(f'<h3 class="big-font">{output[1]}</h3>', unsafe_allow_html=True)
